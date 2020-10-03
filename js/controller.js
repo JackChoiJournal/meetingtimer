5ef4e48463ea906a4264cf75ed5faccdc40d9152
choty036 <choty036@mymail.unisa.edu.au>
2020-10-03 11:28:31 +0930

    feat: Allowing timer to automatically or manuelly start
    User now can pick to start the next timer automatically,
    or manuelly.
    
    - Added a auto start function
    - Fix delay when timer initial start
    - Modify core.css

---
+++ js/controller.js
@@ -0,0 +1,485 @@
+/*
+ * Organization: UniSA
+ * Subject Tool For Software Development
+ * Group: Thu11_group02
+ * Created Date: 26/09/2020
+ */
+
+/*
+ * The controller of this timer web application.
+ * This major function of this controller is to handle the loop of this application
+ */
+let tasksList = {}; // Contain all the task object
+var listLength = 0; // Current tasksList length
+let totalSecond = 0; // Total second of tasks object inside taskList
+let totalMinute = 0; // Total minute of tasks object inside taskList
+let remainSecond = 0; // Total remain second of tasks object inside taskList
+let isAutoStart = false;
+let timerInterval; // Interval of count down
+var theme = 'light'; // Website current theme
+
+window.onload = function () {
+    init(); // Init all button and task element
+};
+
+
+
+function init() {
+    addTaskToList(); // Initial task list
+    sumTaskTime(); // Initial totalSecond, totalMinute, and remainSecond
+    updateProgressBar(); // Update progress bar
+
+    // Init Bootstrap tooltip
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+    // Binding click event with auto start button
+    let autoStartBtn = document.querySelector("#auto-start")
+    autoStartBtn.addEventListener("click", autoStartButtonClickEventHandler, false)
+
+    //Binding click event with change-theme button
+    let changeThemeBtn = document.querySelector('#change-theme');
+    changeThemeBtn.addEventListener('click', function (event) {
+        // console.log("Passing in theme " + theme);
+
+        changeThemeButtonClickEventHandler(theme);
+    })
+
+    // Binding click event with add task button
+    let addTaskBtn = document.querySelector('#add-task'); // Add task input group button
+    addTaskBtn.addEventListener("click", function (event) {
+        addTaskButtonClickEventHandler(event); // Add new task input group nodes into task body
+    }, false)
+
+    // Binding click event with start task button
+    let startTaskInputBtn = document.querySelector('#start-task'); // Add task input group button
+    startTaskInputBtn.addEventListener("click", function (event) {
+        startTaskButtonClickEventHandler(event); // Add new task input group nodes into task body
+    }, false)
+
+    // Binding click event with pause task button
+    let pauseBtn = document.querySelector("#pause-task")
+    pauseBtn.addEventListener('click', pauseTaskButtonClickEventHandler, false);
+
+    // Binding click event with import task button
+    const inputElement = document.querySelector("#import-task");
+    inputElement.addEventListener("click", importTaskButtonClickEventHandler, false);
+
+    // Binding click event with export task button
+    let exportBtn = document.querySelector("#export-task");
+    exportBtn.addEventListener('click', exportTaskButtonClickEventHandler, false);
+
+    // Binding mouseup event with remove task button
+    let li = document.querySelector("#task-body li:first-child"); // Task li elements
+    if (li) {
+        li.addEventListener('mouseup', function (e) {
+            removeTaskButtonClickEventHandler(e, this);
+        }, false)
+    }
+}
+
+function autoStartButtonClickEventHandler(event){
+    isAutoStart = !isAutoStart; // Modify auto start status
+    // Init Bootstrap tooltip
+
+    $("#auto-start").tooltip('hide'); // Hide the current tooltip
+
+    // Modify auto start button text
+    if(isAutoStart){
+        event.target.innerText = "Auto"
+        event.target.dataset.originalTitle = "Automatically start the next timer after finishing the current timer."
+    }else{
+        event.target.innerText = "Manuel"
+        event.target.dataset.originalTitle = "Clicking on start button to start the next timer."
+    }
+}
+
+function changeThemeButtonClickEventHandler(theme) {
+    let body = $('body');
+    let displayTime = $('#remain-time');
+    let headerText = $('h1');
+    let themeBtn = $('#change-theme');
+    let timerBody = $('#timer__container .card');
+    let taskHeader = $('#task__container .card-header');
+    let taskHeaderText = $('#task__container .card-header span');
+    let taskFooter = $('#task__container .card-footer');
+    let taskFooterHr = $('.card-footer hr');
+    let progress = $('.progress');
+
+
+    if (theme === 'dark') { // Change theme to light 
+        this.theme = 'light';
+        body.removeClass('dark-background');
+        displayTime.removeClass("dark-remain-time");
+
+        headerText.removeClass("dark-header-text border-white");
+        headerText.addClass("border-dark");
+
+        themeBtn.removeClass("fa-sun-o btn-light");
+        themeBtn.addClass("fa-moon-o btn-dark");
+
+        timerBody.css("background-color", "#ffecc7");
+        progress.removeClass("dark-progress");
+
+        taskHeader.css("background-color", "#fafcc2");
+        taskHeaderText.removeClass("text-light");
+        taskHeaderText.addClass("text-dark");
+        taskFooter.css("background-color", "#fafcc2");
+        taskFooterHr.css("border-top", "1px solid black");
+
+
+    } else if (theme === 'light') { // Change theme to dark
+        this.theme = 'dark';
+        body.addClass("dark-background");
+        displayTime.addClass("dark-remain-time");
+
+        headerText.removeClass("border-dark");
+        headerText.addClass("dark-header-text border-white");
+
+        themeBtn.removeClass("fa-moon-o btn-dark");
+        themeBtn.addClass("fa-sun-o btn-light");
+
+        timerBody.css("background-color", "#ececec");
+        progress.addClass("dark-progress");
+
+        taskHeader.css("background-color", "#053f5e");
+        taskHeaderText.removeClass("text-dark");
+        taskHeaderText.addClass("text-light");
+        taskFooter.css("background-color", "#053f5e");
+        taskFooterHr.css("border-top", "1px solid white");
+    }
+}
+
+function importTaskButtonClickEventHandler() {
+    let inputNode = document.createElement("input");
+    inputNode.type = "file";
+    inputNode.accept = "text/csv";
+    inputNode.onchange = function () {
+        // Init file content
+        const file = this.files[0]; // First file
+
+        // Return if no file
+        if (file === undefined) {
+            return
+        }
+
+        // Return if file is not acceptable
+        const isAcceptFileType = file.type === "application/vnd.ms-excel"; // Check file type
+        const isAcceptExtension = file.name.split('.').slice(-1)[0] === "csv"; // Check file extension
+        if (!isAcceptFileType || !isAcceptExtension) {
+            return;
+        }
+
+        // Read csv content and add task onto page
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            let tasks = [];
+            e.target.result.split('\n').forEach(function (task) {
+                let info = task.split(',');
+                let title = info[0]; // Task title
+                let minute = info[2]; // Task minute
+
+                createTaskNode(title, minute); // Create task li node
+                addTaskToList(); // Add new task to task list[global]
+            });
+        }
+        reader.readAsText(file, "UTF-8");
+    };
+    inputNode.click(); // Fire click event to start import action
+}
+
+function exportTaskButtonClickEventHandler(event) {
+    // Init csv content
+    let taskContent = []; // task info content
+    Object.entries(tasksList).forEach(function ([taskID, taskObj], index) {
+        let title = taskObj.title
+        let second = taskObj.timer.second
+        let minute = taskObj.timer.minute
+
+        taskContent.push(`${title}, ${second}, ${minute}`)
+    }); // Append task info to array
+    taskContent = taskContent.join("\n");
+
+    // Download event
+    let downloadNode = document.createElement('a');
+    downloadNode.onclick = function () {
+        let blob = new Blob([taskContent], {
+            type: "text/csv"
+        }); // Object transfer
+        downloadNode.href = URL.createObjectURL(blob); // Create URL
+        downloadNode.download = "task.csv" // File name
+        downloadNode.hidden = true; // Hide the node
+    }
+    downloadNode.click(); // Fire download event
+}
+
+function startTaskButtonClickEventHandler(event, elem) {
+    startCountDown(); // Start the count down
+}
+
+function pauseTaskButtonClickEventHandler() {
+    // Stop task timer
+    Object.entries(tasksList).forEach(function ([taskID, taskObj], index) {
+        // Return if not first task's timer
+        if (index !== 0) {
+            return;
+        }
+
+        taskObj.timer.stop(); // Stop timer
+    });
+
+    clearInterval(timerInterval); // Clear count down interval
+}
+
+function addTaskButtonClickEventHandler(event) {
+    /*
+     * Handle the event when add button is pressed.
+     * Major job is create a task li node, and update tasksList[global].
+     */
+    createTaskNode(); // Create task li node
+    addTaskToList(); // Add new task to task list[global]
+}
+
+function removeTaskButtonClickEventHandler(event, elem) {
+    if (event.target.classList.contains("task-remove-btn")) {
+        event.preventDefault(); // Stop event for further action
+        let taskID = elem.parentNode.parentNode.parentNode.parentNode.id // Task id
+        elem.parentNode.parentNode.parentNode.parentNode.remove(); // Remove task node from html node
+        document.querySelector("#remain-time").textContent = "00:00" // Render page timer
+        let isStart = tasksList[taskID].timer.isStart; // Is this timer started count down
+        document.querySelector("#progress-percentage").innerText = "0%"; // Update progress bar text
+        document.querySelector("#progress-time").setAttribute("aria-valuenow", "0");
+        document.querySelector("#progress-time").style.width = "0";
+
+        // Stop started timer
+        if (isStart) {
+            tasksList[taskID].timer.stop(); // Stop the timer
+            clearInterval(timerInterval); // Stop the count down interval
+        }
+
+        delete tasksList[taskID]; // Remove task from task list[global]
+
+        listLength--; // Reduce tasksList length 
+        if (listLength == 3) {
+            $("#task-body").css("height", "17.2rem"); // Change task-body back to default height
+        }
+
+        // Start the next timer if count down is started, task is not empty and auto start is select
+        if (isAutoStart && isStart && Object.keys(tasksList).length > 0) {
+            startCountDown();
+        }
+    }
+}
+
+function modifyTaskTitleInputEventHandler(event, elem) {
+    if (event.target.classList.contains("task-title")) {
+        let taskID = elem.parentNode.parentNode.parentNode.parentNode.id; // Task id
+        tasksList[taskID].title = elem.value;
+    }
+}
+
+function modifyTaskMinuteInputEventHandler(event, elem) {
+    if (event.target.classList.contains("task-minute")) {
+        let taskID = elem.parentNode.parentNode.parentNode.id // Task id
+        tasksList[taskID].timer.minute = elem.value;
+    }
+}
+
+function startCountDown() {
+    // Return if no task exist
+    let tasks = document.querySelectorAll("#task-body>li");
+    if (!tasks.length) {
+        return;
+    }
+
+    // Return if first task has not specified minute input
+    let minute = document.querySelector("#task-body>li:first-child input:nth-child(2).task-minute").value;
+    if (minute === "") {
+        return;
+    }
+
+    let timers = []; // Timers of each task
+
+    // Add each timer of task to timers array
+    for (const [taskID, task] of Object.entries(tasksList)) {
+        timers.push(task.timer)
+    }
+
+    // Return if first timer is started
+    if (timers[0].isStart) {
+        return
+    }
+
+    countDownProcess(timers);
+
+    // Render page with timer time each second
+    timerInterval = setInterval(function () {
+        countDownProcess(timers);
+    }, 1000)
+}
+
+function countDownProcess(timers){
+    // Stop count down if no timer exist
+    if (!timers.length) {
+        clearInterval(timerInterval);
+        return;
+    }
+
+    timers[0].start() // Start the first timer
+    sumTaskTime(); // Update the total time of all task
+    updateProgressBar(); // Update progress bar
+
+    // Second left in a minute
+    let second = (timers[0].secondRemain).toString().length < 2 ? ("0" + (timers[0].secondRemain)).slice(-2) : (timers[0].secondRemain);
+    // Minute left
+    let minute = timers[0].minuteRemain.toString().length < 2 ? ("0" + timers[0].minuteRemain).slice(-2) : timers[0].minuteRemain;
+
+    document.querySelector("#remain-time").textContent = minute + ":" + second // Render page timer
+
+    // Remove timer and task if task's remain time is zero
+    if (timers[0].totalSecondRemain === 0) {
+        timers.shift(); // Remove the first timer in timers array
+        document.querySelector("#task-body>li:first-child").remove(); // Remove task il element
+        // Remove the first task from tasksList[global]
+        Object.entries(tasksList).forEach(function ([taskID, taskObj], index) {
+            if (index !== 0) {
+                return
+            }
+            delete tasksList[taskID] // Remove task
+        })
+
+        // Stop count down if auto start is not selected
+        if (!isAutoStart) {
+            clearInterval(timerInterval);
+        }
+    }
+}
+
+function addTaskToList() {
+    /*
+     * Add tasks info into task list[global]
+     */
+    let taskElem = document.querySelectorAll("#task-body li"); // Query all task li element
+
+    // Add all task to global variable task list
+    Array.from(taskElem).map(liElem => {
+        // Return if task already exist
+        if (tasksList.hasOwnProperty(liElem.id)) {
+            return;
+        }
+        let taskForm = liElem.querySelector("form"); // Form element of task
+        let titleElem = taskForm.elements.namedItem("title"); // Title input element of task
+        let minuteElem = taskForm.elements.namedItem("minute"); // Minute input element of task
+
+        let title = titleElem.value // Task title
+        let minute = Timer.cleanInteger(minuteElem.value) // Task minute
+        tasksList[liElem.id] = new Task(title, 0, minute); // Add Task to task list
+    });
+}
+
+function sumTaskTime() {
+    /*
+     * Calculate and update the total minute, second, and remain second of all task inside task list
+     */
+    let tempMinutes = 0;
+    let tempSeconds = 0;
+    let tempRemainSecond = 0;
+
+    // Sum the total second, minute, and remain second of current tasks in task list[global]
+    for (const [taskID, task] of Object.entries(tasksList)) {
+        tempMinutes += task.timer.minute;
+        tempSeconds += task.timer.second;
+        tempRemainSecond += task.timer.totalSecondRemain;
+    }
+
+    totalMinute = tempMinutes;
+    totalSecond = tempSeconds;
+    remainSecond = tempRemainSecond;
+}
+
+function updateProgressBar() {
+    /*
+     * Update the progress bar by current timer's time
+     */
+
+    // Fetch the percentage of the current timer's remain second
+    let leftTimePercentage = 0;
+    Object.entries(tasksList).forEach(function ([taskID, taskObj], index) {
+        if (index !== 0) {
+            return;
+        }
+
+        // Calculate the percentage of time left
+        if (taskObj.timer.totalSecond !== 0) {
+            leftTimePercentage = (taskObj.timer.totalSecondRemain / taskObj.timer.totalSecond * 100).toFixed(2);
+        } else {
+            leftTimePercentage = 0;
+        }
+    });
+
+    // Update progress bar text
+    document.querySelector("#progress-percentage").innerText = leftTimePercentage + "%";
+
+    // Update progress bar width
+    let progressBar = document.querySelector("#progress-time");
+    progressBar.setAttribute("aria-valuenow", leftTimePercentage.toString());
+    progressBar.style.width = leftTimePercentage + "%";
+    progressBar.style.background = theme === 'light' ? "#0d7377" : "white";
+}
+
+function createTaskNode(title = "", minute = 0) {
+    /*
+     * Create new task input group node and
+     * add the node as a child into the end of the task ul element.
+     *
+     * @param {string} newTaskID The ID of the new task
+     */
+    // Return is minute is not a number
+    minute = parseInt(minute);
+    if (isNaN(minute)) {
+        return;
+    }
+    listLength++;
+    // console.log("Task number: " + listLength);
+
+    if (listLength == 4) {
+        $("#task-body").css("height", "auto");
+    }
+    // Create li element for new task input group
+    let lastTask = document.querySelector("#task-body li:last-child"); // Query the last task element
+    let newTaskID = lastTask ? "task_" + (parseInt(lastTask.id.slice(-1)) + 1) : "task_1" // The ID for new task
+
+    let liElem = document.createElement("li");
+    liElem.className = "list-group-item";
+    liElem.id = newTaskID;
+    liElem.innerHTML = '<form>' +
+        '<div class="input-group">' +
+        '<div class="input-group-prepend mr-1">' +
+        `<input type="text" name="title" class="form-control shadow-sm task-title" value="${title}" placeholder="Title" required>` +
+        '</div>' +
+        `<input type="number" name="minute" class="form-control shadow-sm mr-1 task-minute" value="${minute}" min="0" step="1" placeholder="Minute" required>` +
+        '<div class="input-group-append">' +
+        '<a class="btn btn-secondary shadow-sm task-remove-btn">Remove</a>' +
+        '</div>' +
+        '</div>' +
+        '</form>'; // Task title, time and add button bodies
+
+    // Add event listener to the task remove button
+    liElem.querySelector(".task-remove-btn").addEventListener('mouseup', function (e) {
+        removeTaskButtonClickEventHandler(e, this);
+    }, false);
+
+    // Add event listener to the task title input
+    liElem.querySelector(".task-title").addEventListener('input', function (e) {
+        modifyTaskTitleInputEventHandler(e, this);
+    }, false);
+
+    // Add event listener to the task minute input
+    liElem.querySelector(".task-minute").addEventListener('input', function (e) {
+        modifyTaskMinuteInputEventHandler(e, this);
+    }, false);
+
+    let taskBody = document.querySelector("#task-body");
+    taskBody.appendChild(liElem); // Add task input group node into task ul element
+}
---
+++ js/timer.js
@@ -0,0 +1,201 @@
+/*
+ * Organization: UniSA
+ * Subject Tool For Software Development
+ * Group: Thu11_group02
+ * Author: choty036@mymail.unisa.edu.au
+ * Created Date: 16/08/2020
+ */
+
+/*
+* The core functions of the Timer class are start() and stop().
+* Once a timer object call start(), the timer object start count down.
+* on the contrary, calling stop() stop the timer counting down.
+*
+*  The public fields
+* @param {number} minute Allow to get or set. The minute remain.
+* @param {number} second Allow to get or set. The second remain. Beware this field is not total second, it doesn't contain the minute with it.
+* @param {number} totalSecond Allow to get ONLY The total second. Basically this field combine the second and minute fields.
+* @param {number} secondRemain Allow to get ONLY. The remaining second of timer
+* @param {number}  isStart Allow to get ONLY. It's the status of the timer
+*/
+class Timer {
+    #_minute = 0; // The original minute of the timer
+    #_second = 0; // The original second of the timer. Be careful it's not the total second of the timer
+    #_totalSecondRemain = 0; // The total second left of the timer
+    #_isStart = false; // Timer status
+    #_interval; // The setInterval() that time the total second
+
+    constructor(second = 0, minute = 0, start = false) {
+        this.second = second;
+        this.minute = minute;
+        this.#_isStart = start;
+        this.totalSecondRemain = this.second + Timer.minuteToSecond(this.minute);
+
+        if (this.isStart) {
+            this.start(); // Start timer
+        }
+    }
+
+    get second() {
+        return this.#_second;
+    }
+
+    /* Warning: Changing second will cause secondRemain reset with given value */
+    set second(value) {
+        value = Timer.cleanInteger(value);
+        this.#_second = value;
+        this.#_totalSecondRemain = this.second + this.minute * 60; // Update total second
+    }
+
+    get minute() {
+        return this.#_minute;
+    }
+
+    /* Warning: Changing minute will cause secondRemain reset with given value */
+    set minute(value) {
+        value = Timer.cleanInteger(value);
+        this.#_minute = value;
+        this.#_totalSecondRemain = this.second + this.minute * 60; // Update total second
+    }
+
+    // Getter of total second of the timer
+    get totalSecond() {
+        return this.second + Timer.minuteToSecond(this.minute)
+    }
+
+    // Setter of total second remain of the timer
+    set totalSecondRemain(value) {
+        value = Timer.cleanInteger(value);
+        this.#_totalSecondRemain = value;
+    }
+
+    // Getter of total second remain of the timer
+    get totalSecondRemain() {
+        return this.#_totalSecondRemain;
+    }
+
+    // Getter of second remain of the timer. The current second remain in that moment.
+    get secondRemain() {
+        return Math.floor(this.totalSecondRemain % 60);
+    }
+
+    // Getter of minute remain of the timer. The current minute remain in that moment.
+    get minuteRemain() {
+        return Math.floor(this.totalSecondRemain / 60);
+    }
+
+    // Is the timer started
+    get isStart() {
+        return this.#_isStart;
+    }
+
+    /*
+     * This function converts a number to integer and return a integer
+     *
+     *  @param {number} number Any number type parameter
+     *  @return {number} integer
+     */
+    static cleanInteger(number = 0) {
+        let num = parseInt(number)
+
+        if (isNaN(num) || number < 0) {
+            return 0;
+        }
+
+        return num;
+    }
+
+    /*
+     * This function converts minute to second and return second
+     *  @param {number} minute
+     *  @return {number}
+     */
+    static minuteToSecond(minute = 0) {
+        minute = this.cleanInteger(minute);
+
+        return Math.floor(minute * 60);
+    }
+
+    /*
+        * This function converts second to minute and return minute
+        *
+        *  @param {number} seccond
+        *  @return {number}
+        */
+    static secondToMinute(second = 0) {
+        second = this.cleanInteger(second);
+
+        return Math.floor(second / 60);
+    }
+
+    /*
+        * This function call a setInterval to start timer, and change
+        *  timer start field. The setInerval will stop the timer object
+        *  once the totalSecond of the timer object reach to 0.
+        *
+        *  @param {number} obj Timer object
+        */
+    #startTimer() {
+        // Return if timer already started
+        if (this.#_isStart) return;
+
+        // console.log("Start timer");
+        this.#_isStart = true; // Update timer status
+
+        let obj = this;
+        this.#_interval = setInterval(function () {
+            obj.totalSecondRemain -= 1;
+            // console.log("Current Total Second Left: " + obj.totalSecondRemain);
+
+            if (obj.totalSecondRemain <= 0) {
+                obj.stop();
+            }
+        }, 1000)
+    }
+
+    /*
+        * This function stop timing interval to stop timer,
+        * and change timer start field
+        *  @param {number} obj Time object
+        */
+    #stopTimer() {
+        if (!this.#_isStart) return; // Return if timer not yet start
+
+        // console.log("Stop timer");
+        this.#_isStart = false; // Update timer status
+        clearInterval(this.#_interval); // Stop the interval that counting the second
+    }
+
+    /*
+        * This function stop timing interval to stop timer,
+        * and change timer start field
+        *  @param {number} obj Time object
+        */
+    #resetTimer() {
+        // console.log("Reset timer");
+        // Trigger setter of timer minute and second will force the totalSecondLeft reset itself with the time
+        this.minute = this.#_minute;
+        this.second = this.#_second;
+    }
+
+    /*
+        * This function start timer
+        */
+    start() {
+        this.#startTimer(); // The real implementation start timing
+    }
+
+    /*
+        * This function stop timer
+        */
+    stop() {
+        this.#stopTimer(); // The real implementation stop timing
+    }
+
+    /*
+        * This function reset timer
+        */
+    reset() {
+        this.#resetTimer(); // The real implementation stop timing
+    }
+}
